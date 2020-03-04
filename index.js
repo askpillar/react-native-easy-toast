e421f7391861d2fdc3202b6d8f4bc53347d6550e
Felipe Arosemena <felipe@pillar.app>
2020-03-04 07:38:53 -0500

    Bye `point-events` none

--- index.js
+++ index.js
@@ -106,7 +106,6 @@ export default class Toast extends Component {
         const view = this.state.isShow ?
             <View
                 style={[styles.container, { top: pos }]}
-                pointerEvents="none"
             >
                 <Animated.View
                     style={[styles.content, { opacity: this.state.opacityValue }, this.props.style]}
